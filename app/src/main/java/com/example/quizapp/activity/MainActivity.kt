eadd547305b9dc90313b52f4cdb8ac5e10407185
Pobhenrhaj <alenprem4@yahoo.com>
2023-04-03 17:47:06 +0800

    Commit with main(Not complete)

--- app/src/main/java/com/example/quizapp/activity/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/activity/MainActivity.kt
@@ -2,11 +2,26 @@ package com.example.quizapp.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.quizapp.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.nav_host_fragment)
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
